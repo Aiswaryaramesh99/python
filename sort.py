a5bb60d191d39929a9ee81e50a534a01e172b04d
Aiswaryaramesh99 <120564321+Aiswaryaramesh99@users.noreply.github.com>
2023-08-02 15:42:02 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mydatabase1"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+mydb.commit()
+for x in myresult:
+  print(x)
